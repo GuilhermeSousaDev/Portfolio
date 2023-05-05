8165b41b0a858adcc39fbf8872f34968feb8e00a
Guilherme Augusto <a.guilhermerocha250@gmail.com>
2023-05-05 10:30:26 -0300

    Project See Button

--- src/app/Projects/index.tsx
+++ src/app/Projects/index.tsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
-import { useTheme } from "@mui/material";
+import Button from "@mui/material/Button";
 import { projectsData } from "../../config/projectsData";
+import { useAppSelector } from "../../hooks/useAppSelector";
 
 export default function Projects() {
-  const theme = useTheme();
+  const isThemeDark = useAppSelector((state) => state.theme.default) === "dark";
+
+  const [buttonIndex, setButtonIndex] = useState<number | null>(null);
+
+  const handleShowButton = (index: number) => {
+    setButtonIndex(index);
+  }
 
   return (
     <Box>
-      <Typography 
-        variant="h3" 
-        color="text.primary" 
-        sx={{ textAlign: 'center', mb: 5 }}
+      <Typography
+        variant="h3"
+        color="text.primary"
+        sx={{ textAlign: "center", mb: 5 }}
       >
         Projects
       </Typography>
@@ -21,24 +29,51 @@ export default function Projects() {
         display="flex"
         flexWrap="wrap"
         justifyContent="center"
+        sx={{ bgcolor: "white", borderRadius: 3 }}
       >
-        {projectsData.map((project) => (
-          <>
+        {projectsData.map((project, index) => (
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
             <Box
               component="img"
+              key={index}
+              onMouseOver={() => handleShowButton(index)}
+              onMouseLeave={() => setButtonIndex(null)}
               sx={{
                 width: 170,
                 height: 100,
                 borderRadius: 3,
+                border: "1px solid black",
+                boxShadow: "2px 2px 2px #151B21",
                 m: 5,
-                '&:hover': {
-                  cursor: 'pointer',
-                  filter: 'drop-shadow(2px 2px 2px #5135F0)',
+                "&:hover": {
+                  cursor: "pointer",
+                  filter: `drop-shadow(2px 2px 2px ${
+                    isThemeDark ? "#5135F0" : "#E66D32"
+                  })`,
+                  boxShadow: "0px 0px 0px white",
+                  
                 },
               }}
               src={project.screenshot}
             />
-          </>
+            <Button
+              onMouseOver={() => handleShowButton(index)}
+              variant="contained"
+              sx={{ 
+                transform: "translateY(-105px)", 
+                width: 100, 
+                visibility: index === buttonIndex ? 'visible' : 'hidden',
+            }}
+            >
+              See
+            </Button>
+            
+          </Box>
         ))}
       </Box>
     </Box>
---
+++ src/components/MuiCustomComponents/ProjectModal.tsx
@@ -0,0 +1,14 @@
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+
+const style = {}
+
+export default function ProjectModal() {
+    return (
+        <Modal open={false}>
+            <Box>
+
+            </Box>
+        </Modal>
+    )
+}
